c694108665299df0bba687ffcb5ace54fafa9cb8
shashankreddybanda <shashank24140544@gmail.com>
2023-12-12 12:50:45 -0700

    "added"

---
+++ app/create/page.tsx
@@ -0,0 +1,5 @@
+export default function Create(){
+    return (
+        <div className="font-bold text-3xl">Create</div>
+    )
+}
---
+++ app/events/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+export default function Events(){
+return (
+	<div>
+	<p>Events</p>
+	<Link href='/'>Home </Link>
+	</div>
+)
+}
